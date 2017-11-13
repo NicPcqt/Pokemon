1fba5b7309c46a92f9c3498a97c7c795ca27ed46
Nicolas Pocquet <nicolas.pocquet@outlook.fr>
2017-11-13 14:51:13 +0100

    Initial commit

---
+++ public/index.php
@@ -0,0 +1,12 @@
+
+<?php
+    require_once('../resource/header.php')
+    
+    
+    require_once('../resource/function.php');
+    $errors = [];
+
+    affichepokemon():
+
+    require_once('../resource/footer.php')
+?>
---
+++ resource/function.php
@@ -0,0 +1,9 @@
+<?php
+require_once '../resource/database.php';
+
+function pokemon(&$errors = array()){
+    
+}
+
+
+?>
