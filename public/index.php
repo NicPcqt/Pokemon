d56ef2e7bfb054ea124f0bdc7fc8853c9f41c267
Nicolas Pocquet <nicolas.pocquet@outlook.fr>
2017-11-13 15:00:07 +0100

    initial commit

--- public/index.php
+++ public/index.php
@@ -1,12 +1,11 @@
 
 <?php
-    require_once('../resource/header.php')
-    
-    
-    require_once('../resource/function.php');
-    $errors = [];
-
-    affichepokemon():
-
-    require_once('../resource/footer.php')
-?>
+// Entête HTML ce require permet de charger toutes les balises d'en-tête de la page HTML
+require_once('header.php');
+// Fonctions de bases
+require_once('../resources/function.php');
+$errors = [];
+// Affichage du lien d'insertion
+affichePokemon();
+// Fin du HTML
+require_once('footer.php');
--- resource/function.php
+++ resource/function.php
@@ -1,9 +1,108 @@
 <?php
-require_once '../resource/database.php';
-
-function pokemon(&$errors = array()){
-    
+// Gestion de la base de donnée
+require_once('database.php');
+/*
+ * formIsSubmit : test si un fomulaire a été soumis
+ */
+function formIsSubmit($form_name) {
+  return (isset($_POST[$form_name]) ? $_POST[$form_name] : '0') === '1';
 }
-
-
+function getVal($value, $default = '') {
+  return isset($value) ? $value : $default;
+}
+function showMessage($message, $type = 'alert-success') {
+  $html = "
+  <div class=\"alert $type alert-dismissible fade show\" role=\"alert\">
+    <button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\">
+      <span aria-hidden=\"true\">&times;</span>
+    </button>
+    " . htmlspecialchars($message) . "
+  </div>
+  ";
+  echo $html;
+}
+function attaque($nom_pokemon1, &$pokemon1, $nom_pokemon2, &$pokemon2) {
+  // $tour est initialisée à 0 et conservera sa dernière modification à chaque appel de la fonction grâçe au mot clé static
+  static $tour = 0;
+  echo "<h2> Tour : " . ++$tour . " à " . date('H:i:s') . "</h2>";
+  // pokemon1 attaque pokemon2
+  echo "<h3>$nom_pokemon1 attaque $nom_pokemon2</h3>";
+  if ($pokemon1['attaque'] >= $pokemon2['defense']) {
+    // L'attaque est supérieure à la défense : pokemon1 touche
+    $coup = $pokemon1['attaque'] - $pokemon2['defense'] + 1; // La valeur du coup est la différence entre l'attaque et la défense
+    $pokemon2['pv'] -= $coup;
+    if ($pokemon2['pv'] < 0)
+      $pokemon2['pv'] = 0;
+    echo "<p>$nom_pokemon2 perd $coup PV, il lui reste " . $pokemon2['pv'] . " PV</p>";
+  } else {
+    // La défense est supérieure à l'attaque, pokemon1 prend la moitié du coup et la défense baisse un peu
+    $coup = ($pokemon2['defense'] - $pokemon1['attaque']) / 2;
+    $pokemon1['pv'] -= $coup;
+    if ($pokemon1['pv'] < 0)
+      $pokemon1['pv'] = 0;
+    $pokemon2['defense'] -= 1;
+    echo "<p>$nom_pokemon2 perd 1 Points de défense, il lui reste " . $pokemon2['defense'] . " Points de défense</p>";
+    echo "<p>$nom_pokemon1 râte son attaque ! Il perd $coup Points de vie, il lui reste " . $pokemon1['pv'] . " Points de vie</p>";
+  }
+  if ($pokemon2['pv'] <= 0)
+    echo "<p>$nom_pokemon2 est KO !</p>";
+  if ($pokemon1['pv'] <= 0)
+    echo "<p>$nom_pokemon1 est KO !</p>";
+}
+function genereTable($query) {
+  $table = "";
+  while ($result = $query->fetch()) {
+    // Première ligne : affichage des titres de colonnes
+    if ($table == "") {
+      $table = "
+  <table class=\"table table-hover table-responsive-sm\">
+    <caption>Liste de tous les pokemons existants</caption>
+    <thead>
+      <tr>
+        <th scope=\"col\">
+        </th>
+        <th scope=\"col\">
+        " . implode('</th><th scope=\"col\">', array_keys($result)) . "
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      ";
+    }
+    // Ajout d'une ligne dans la table
+    $table .= "
+      <tr>
+        <td scope=\"row\">
+          <a onclick=\"formSubmit('deletePokemon', 'id_delete', '" . $result['id'] . "');\"><i class=\"fa fa-trash-o fa-fw\" aria-hidden=\"true\"></i></a>
+        </td>
+        <td>
+        " . implode('</td><td>', $result) . "
+        </td>
+      </tr>
+    ";
+  }
+  if($table == "") {
+    null;//$errors[] = "Aucune ligne trouvée";
+  } else {
+    $table .= "
+    </tbody>
+  </table>
+    ";
+  }
+  return $table;
+}
+function affichePokemon() {
+  // Connexion à la base
+  if (!$db = connexion($msg))
+    die("Erreur : " . implode($msg));
+  // Affichage des pokemons
+  if (!$query = $db->query('
+  SELECT pokemon.id, numero, nom, experience, vie, defense, attaque, nom_proprietaire
+    FROM pokemon
+      LEFT JOIN pokedex ON (id_pokedex = pokedex.id)
+  ')) {
+    $errors[] = "Erreur lors de la création de la requête";
+  }
+  echo genereTable($query);
+}
 ?>
