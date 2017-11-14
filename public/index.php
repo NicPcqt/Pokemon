ea84f91c044c19c94ac64db5a76502b270424b6d
Nicolas Pocquet <nicolas.pocquet@outlook.fr>
2017-11-14 15:25:27 +0100

    formulaire inscritption

--- public/index.php
+++ public/index.php
@@ -1,11 +1,61 @@
-
 <?php
 // Entête HTML ce require permet de charger toutes les balises d'en-tête de la page HTML
-require_once('header.php');
+require_once('../resource/header.php');
 // Fonctions de bases
-require_once('../resources/function.php');
+require_once('../resource/function.php');
 $errors = [];
-// Affichage du lien d'insertion
-affichePokemon();
+// Gestion des formulaires
+if (formIsSubmit('signin_form')) {
+  // Traitement du formulaire de connexion
+}
+if (formIsSubmit('signup_form')) {
+  // Traitement du formulaire d'inscription
+}
+?>
+
+<div class="container form-signin">
+  <ul class="nav nav-tabs nav-fill" id="myTab" role="tablist">
+    <li class="nav-item">
+      <a class="nav-link active" id="signin-tab" data-toggle="tab" href="#signin" role="tab" aria-controls="signin" aria-selected="true">Se connecter</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" id="signup-tab" data-toggle="tab" href="#signup" role="tab" aria-controls="signup" aria-selected="false">S'inscrire</a>
+    </li>
+  </ul>
+
+  <div class="tab-content" id="myTabContent">
+    <div class="tab-pane fade show active" id="signin" role="tabpanel" aria-labelledby="signin-tab">
+      <form method="post">
+        <input type="hidden" name="signin_form" value="1"/>
+        <label for="email" class="sr-only">Adresse Email</label>
+        <input type="email" id="email" name="email" class="form-control" placeholder="Adresse email" required autofocus>
+        <label for="password" class="sr-only">Mot de passe</label>
+        <input type="password" id="password" name="password" class="form-control" placeholder="Mot de passe" required>
+        <div class="checkbox">
+          <label>
+            <input type="checkbox" value="remember-me">Se souvenir
+          </label>
+        </div>
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Connexion</button>
+      </form>
+    </div>
+
+    <div class="tab-pane fade" id="signup" role="tabpanel" aria-labelledby="signup-tab">
+      <form method="post">
+        <input type="hidden" name="signup_form" value="1"/>
+        <label for="email" class="sr-only">Adresse Email</label>
+        <input type="email" id="email" name="email" class="form-control" placeholder="Adresse email" required autofocus>
+        <label for="password" class="sr-only">Mot de passe</label>
+        <input type="password" id="password" name="password" class="form-control" placeholder="Mot de passe" required>
+        <label for="confirmPassword" class="sr-only">Confirmez</label>
+        <input type="password" id="confirmPassword" name="confirmPassword" class="form-control" placeholder="Confirmez le mot de passe" required>
+        <label for="nom" class="sr-only">Nom</label>
+        <input type="password" id="nom" name="nom" class="form-control" placeholder="Nom" required>
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Inscription</button>
+      </form>
+    </div>
+</div> <!-- /container -->
+
+<?php
 // Fin du HTML
-require_once('footer.php');
+require_once('../resource/footer.php');
--- resource/header.php
+++ resource/header.php
@@ -1,10 +1,20 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <title>Pokéhome</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="css/style.css" rel="stylesheet">
-    </head>
-    <body>
-    
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Pokemon APP</title>
+
+    <link href="components/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="components/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+    <link href="css/style.css" rel="stylesheet" type="text/css">
+
+    <script src="components/jquery/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+    <script src="components/bootstrap/js/bootstrap.min.js"></script>
+</head>
+<body>
+  <div class="text-center">
+    <img src="img/pokemon.png" alt="" style="width: 30%;">
+  </div>
