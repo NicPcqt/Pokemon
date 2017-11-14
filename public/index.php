e86cdd947a37862df301d8284462626e383cc44f
Nicolas Pocquet <nicolas.pocquet@outlook.fr>
2017-11-14 17:28:40 +0100

    autnetificatiopn part 1

--- public/index.php
+++ public/index.php
@@ -1,16 +1,106 @@
 <?php
-// Entête HTML ce require permet de charger toutes les balises d'en-tête de la page HTML
-require_once('../resource/header.php');
+session_start(); // démarrer la gestion de session PHP
 // Fonctions de bases
-require_once('../resource/function.php');
+require_once('../resources/function.php');
 $errors = [];
+$form_errors = [];
+/*
+// Envoi d'un mail :
+// Dans php.ini configurer sendmail_path = "C:\xampp\mailtodisk\mailtodisk.exe"
+// et sendmail_from="notification@xampp.com"
+if (!mail($email, "test de mail", "Bienvenue sur XAMPP"))
+  showMessage("Mail en erreur");
+*/
+// Connexion à la base de donnée
+if (!$db = connexion($errors)) {
+  die ("Erreur de connexion à la base : " . implode($errors) . "\n<br>Contactez un administrateur");
+}
 // Gestion des formulaires
 if (formIsSubmit('signin_form')) {
   // Traitement du formulaire de connexion
+  // Récupération des valeurs du formulaire
+  $email = $_POST['email'];
+  $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+  $remember = intVal($_POST['remember-me'] ?? 0);
+  // Vérification des saisies
+  if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    $form_errors['email'] = 'Adresse email invalide !';
+  }
+  if (empty($password)) {
+    $form_errors['password'] = 'Mot de passe non renseigné !';
+  }
+  // S'il n'y a pas eu d'erreur dans le formulaire
+  if (count($form_errors) == 0) {
+    // Récupération du compte utilisateur
+    $query = $db->prepare("SELECT id, email, nom, password FROM dresseur WHERE email = :email");
+    $query->bindValue(':email', $email, PDO::PARAM_STR);
+    $query->execute();
+    $user = $query->fetch();
+    if (!$user || password_verify($password, $user['password'])) {
+      // Ne soyons pas trop précis sur l'errreur pour éviter de donner des indices aux attaquants
+      $form_errors['email'] = "Email non trouvé ou mot de passe invalide";
+    } else {
+      // Ici l'email et le mot de passe sont validés
+      $_SESSION["id"] = $user['id'];
+      $_SESSION['ip'] = $_SERVER['REMOTE_ADDR'];
+      // Mise en place d'un cookie de session
+      // Ce code est à faire avant l'affichage du HTML ou une redirection
+      //$_SESSION['token'] = sha1(time() . rand() . $_SERVER['SERVER_NAME']);
+      //setcookie('token', $_SESSION['token']);
+      // In practice, you'd want to store this token in a database with the username so it's persistent.
+      showMessage("Connexion réussie");
+      header("location: liste.php");
+    }
+  }
 }
 if (formIsSubmit('signup_form')) {
   // Traitement du formulaire d'inscription
+  // Récupération des valeurs du formulaire
+  $email = $_POST['email'];
+  $password = $_POST['password'];
+  $confirmPassword = $_POST['confirmPassword'];
+  $hashPassword = password_hash($_POST['password'], PASSWORD_DEFAULT);
+  $nom = $_POST['nom'];
+  // Vérification des saisies
+  if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    $form_errors['email'] = 'Adresse email invalide !';
+  }
+  if (empty($password)) {
+    $form_errors['password'] = 'Mot de passe non renseigné !';
+  }
+  if (empty($confirmPassword)) {
+    $form_errors['confirmPassword'] = 'Mot de passe de confirmation non renseigné !';
+  }
+  if (empty($nom)) {
+    $form_errors['nom'] = 'Nom non renseigné !';
+  }
+  // S'il n'y a pas eu d'erreur dans le formulaire
+  if (count($form_errors) == 0) {
+    // Vérification de l'email en base de donnée
+    $query = $db->prepare("SELECT id, email, nom, password FROM dresseur WHERE email = :email");
+    $query->bindValue(':email', $email, PDO::PARAM_STR);
+    $query->execute();
+    $users = $query->fetchAll();
+    if (count($users) > 0) {
+      $form_errors['email'] = "Email déjà pris, connectez vous au lieu de vous inscrire";
+    } else {
+      // Ici tout est valide, l'insertion peut être faite
+      $query = $db->prepare("
+        INSERT INTO dresseur(email,  nom,  password)
+          VALUES            (:email, :nom, :password)"
+      );
+      $query->bindValue(':email', $email, PDO::PARAM_STR);
+      $query->bindValue(':nom', $nom, PDO::PARAM_STR);
+      $query->bindValue(':password', $hashPassword, PDO::PARAM_STR);
+      if (!$query->execute())
+        showMessage("Erreurs lors de l'inscription : " . implode($query->errorInfo()), 'alert-danger');
+      else
+        showMessage("Inscription OK");
+    }
+  }
 }
+// Entête HTML ce require permet de charger toutes les balises d'en-tête de la page HTML
+require_once('header.php');
 ?>
 
 <div class="container form-signin">
@@ -28,12 +118,14 @@ if (formIsSubmit('signup_form')) {
       <form method="post">
         <input type="hidden" name="signin_form" value="1"/>
         <label for="email" class="sr-only">Adresse Email</label>
-        <input type="email" id="email" name="email" class="form-control" placeholder="Adresse email" required autofocus>
+        <input type="email" id="email" name="email" class="form-control <?php echo isset($form_errors['email']) ? 'is-invalid' : '' ?>" placeholder="Adresse email" required autofocus>
+        <?php echo isset($form_errors['email']) ? '<div class="invalid-feedback">' . $form_errors['email'] . '</div>' : '' ?>
         <label for="password" class="sr-only">Mot de passe</label>
         <input type="password" id="password" name="password" class="form-control" placeholder="Mot de passe" required>
+        <?php echo isset($form_errors['password']) ? '<div class="invalid-feedback">' . $form_errors['password'] . '</div>' : '' ?>
         <div class="checkbox">
           <label>
-            <input type="checkbox" value="remember-me">Se souvenir
+            <input type="checkbox" name="remember-me" value="1">Se souvenir
           </label>
         </div>
         <button class="btn btn-lg btn-primary btn-block" type="submit">Connexion</button>
@@ -50,7 +142,7 @@ if (formIsSubmit('signup_form')) {
         <label for="confirmPassword" class="sr-only">Confirmez</label>
         <input type="password" id="confirmPassword" name="confirmPassword" class="form-control" placeholder="Confirmez le mot de passe" required>
         <label for="nom" class="sr-only">Nom</label>
-        <input type="password" id="nom" name="nom" class="form-control" placeholder="Nom" required>
+        <input type="text" id="nom" name="nom" class="form-control" placeholder="Nom" required>
         <button class="btn btn-lg btn-primary btn-block" type="submit">Inscription</button>
       </form>
     </div>
@@ -58,4 +150,4 @@ if (formIsSubmit('signup_form')) {
 
 <?php
 // Fin du HTML
-require_once('../resource/footer.php');
+require_once('footer.php');
